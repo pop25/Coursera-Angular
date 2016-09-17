396ddd4f751ef1d218dde64caa2a630d49de197f
unknown <popadicd72@gmail.com>
2016-09-17 15:21:05 +0200

    Final

--- module1-sol/app.js
+++ module1-sol/app.js
@@ -11,6 +11,8 @@
       
       $scope.meal = "";
       $scope.message = "Result";
+      $scope.style="";
+      $scope.id = "";
       
     $scope.displayMessage  = function () {
         var mealWords = $scope.meal;
@@ -22,16 +24,20 @@
             if (!(mealWords[i].length === 0)) {
                 count += 1;
                 console.log(count);
+               
             }
             console.log("count is " + count);
             if  ((count > 0) && (count <= 3)){
                 $scope.message = "Enyoj"
+                $scope.style = "style1";
             }
             else if (count > 3) {
                 $scope.message="To much!!!"
+                $scope.style = "style2";
             }
             else {
                 $scope.message= "Error! Please input some dishes"
+                 $scope.id = "style3";
             }
         }
       // return $scope.message
