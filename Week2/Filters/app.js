23c23244be4d68945c2a90d4a4aa4d2835f5f467
unknown <popadicd72@gmail.com>
2016-09-21 22:53:20 +0200

    mywork

---
+++ Week2/Filters/app.js
@@ -0,0 +1,47 @@
+(function () {
+    'use strict';
+    
+    angular.module("MsgApp", [])
+    
+    .controller('MsgController', MsgController)
+    .filter('loves', LovesFilter)
+    .filter('truth', TruthFilter);
+    
+    MsgController.$inject = ['$scope', 'lovesFilter'];
+    
+    function MsgController ($scope, lovesFilter) {
+        $scope.stateOfBeing = 'hungry';
+        
+        $scope.sayMessage = function () {
+         var msg = "Yakov likes to eat healthy snacks at night";
+            return msg;
+        };
+        
+        $scope.sayLovesFilter = function () {
+         var msg = "Yakov likes to eat healthy snacks at night";
+            msg = lovesFilter(msg)
+            return msg;
+        };
+        
+        $scope.feedYaakov = function () {
+            $scope.stateOfBeing = 'fed';
+        };
+    }
+    
+    function LovesFilter() {
+        return function (input) {
+          input = input || "";  
+          input = input.replace("likes", "loves");
+        return input;    
+        };
+    }
+    
+    function TruthFilter() {
+      return function (input, target, replace) {
+          input = input || "";
+          input = input.replace(target, replace);
+      return input;      
+      };
+    };
+    
+})();
